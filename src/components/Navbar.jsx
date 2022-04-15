821b2ca54cf02366ee7f05e1b0a07117e9816e81
Vishal <vishalkumar3474@gmail.com>
2022-04-15 17:57:21 +0530

    g

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,85 @@
-import React, {useState} from 'react';
-import { Link, animateScroll as scroll, } from 'react-scroll'
+import React, { useState } from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
 
-
-import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false)
-    const handleClick = () => setNav(!nav)
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
 
-    const handleClose =()=> setNav(!nav)
+  const handleClose = () => setNav(!nav);
 
   return (
-    <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
-      <div className='px-2 flex justify-between items-center w-full h-full'>
-        <div className='flex items-center'>
-          <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>BRAND.</h1>
-          <ul className='hidden md:flex'>
-          <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
-          <li><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
-         
-          <li><Link to="platforms" smooth={true} offset={-100} duration={500}>Projects</Link></li>
-          <li><Link to="pricing" smooth={true} offset={-50} duration={500}>Resume</Link></li>
+    <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
+      <div className="px-2 flex justify-between items-center w-full h-full">
+        <div className="flex items-center">
+          <h1 className="text-3xl font-bold mr-4 sm:text-4xl">VISHAL.</h1>
+          <ul className="hidden md:flex">
+            <li>
+              <Link to="home" smooth={true} duration={500}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="about" smooth={true} offset={-200} duration={500}>
+                About
+              </Link>
+            </li>
+
+            <li>
+              <Link to="platforms" smooth={true} offset={-100} duration={500}>
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link to="pricing" smooth={true} offset={-50} duration={500}>
+                Resume
+              </Link>
+            </li>
           </ul>
         </div>
-       
       </div>
 
-      <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
-          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="home" smooth={true} duration={500}>Home</Link></li>
-          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
-        
-          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="platforms" smooth={true} offset={-100} duration={500}>Platforms</Link></li>
-          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="pricing" smooth={true} offset={-50} duration={500}>Pricing</Link></li>
+      <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link onClick={handleClose} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link
+            onClick={handleClose}
+            to="about"
+            smooth={true}
+            offset={-200}
+            duration={500}
+          >
+            About
+          </Link>
+        </li>
 
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link
+            onClick={handleClose}
+            to="platforms"
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Platforms
+          </Link>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link
+            onClick={handleClose}
+            to="pricing"
+            smooth={true}
+            offset={-50}
+            duration={500}
+          >
+            Pricing
+          </Link>
+        </li>
       </ul>
     </div>
   );
