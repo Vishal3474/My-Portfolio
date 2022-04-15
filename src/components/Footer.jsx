cab98c5fba495f500c33d87619b8741e163f4aba
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-04-15 17:37:12 +0530

    Update Footer.jsx
    t

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
     <div className='w-full mt-24 bg-slate-900 text-gray-300 py-y px-2'>
         <div className='max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8'>
             
-            <div className='col-span-2 pt-8 text-center md:pt-2'>
+            <div className='col-span-2 pt-8 text-center items-center md:pt-2'>
                 <p className='font-bold uppercase'>Subscribe to our newsletter</p>
                 <p className='py-4'>The latest news, articles, and resources, sent to your inbox weekly.</p>
                 <form className='flex flex-col sm:flex-row'>
