72312e690ef1aa7f7aa5fb9860b2f9137b379639
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-04-15 13:56:47 +0530

    Update App.js
    Ok

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar'
 import Pricing from './components/Pricing';
-import Support from './components/Support'
+
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
-      <Support />
+      
       <AllInOne />
       <Pricing />
       <Footer />
