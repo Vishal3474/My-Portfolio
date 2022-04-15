4e05fa7f23b49830fcb584cb1fb987e7887db491
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-04-15 14:15:12 +0530

    Update AllInOne.jsx
    ok

--- src/components/AllInOne.jsx
+++ src/components/AllInOne.jsx
@@ -5,11 +5,9 @@ const AllInOne = () => {
   return (
     <div name='platforms' className='w-full my-32'>
       <div className='max-w-[1240px] mx-auto px-2'>
-        <h2 className='text-5xl font-bold text-center'>All-In-One Platform</h2>
+        <h2 className='text-5xl font-bold text-center text-indigo-600'>My Recent Works</h2>
         <p className='text-2xl py-8 text-gray-500 text-center'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis,
-          ab. Officia sunt nulla aspernatur culpa, eaque tenetur excepturi
-          nostrum tempore.
+          Here are a few projects I've worked on recently.
         </p>
 
         <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
