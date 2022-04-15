a9297c9e91ef7e05696fe7d92bd668af173e7a69
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-04-15 18:08:01 +0530

    Update Hero.jsx
    text color change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
         <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
             <div className='flex flex-col justify-center md:items-start w-full px-2 py-8'>
                 <p className='text-2xl'>Hi There!</p>
-                <h1 className='py-3 text-5xl md:text-7xl font-bold'> I'M Vishal</h1>
+                <h1 className='py-3 text-5xl text-purple-500 md:text-7xl font-bold'> I'M Vishal</h1>
                
                 
             </div>
