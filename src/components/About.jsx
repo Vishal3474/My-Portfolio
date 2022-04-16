51c6982cf7d86ff7426375eb2ca6a331b9f93c36
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-04-16 12:10:28 +0530

    Update About.jsx
    try

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
         <div className='max-w-[1240px] mx-auto'>
           
     <div className="bg-white shadow overflow-hidden sm:rounded-lg">
-      <div className="px-4 py-5 sm:px-6">
+      <div className="px-4 py-5 text-center sm:px-6">
         <h3 className="text-2xl leading-6 font-medium text-indigo-900">About</h3>
         <p className="mt-1 max-w-2xl text-xl text-indigo-500">Know Who I'M.</p>
       </div>
