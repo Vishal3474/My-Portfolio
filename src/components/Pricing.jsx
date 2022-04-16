2813e093a9a43462076eb69563cbeb89501580a8
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-04-16 12:48:45 +0530

    Update Pricing.jsx
    center

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Pricing = () => {
   return (
     <div name='pricing' className='w-full my-32'  >
-     <h1 class="text-indigo-500 text-4xl font-bold"  >Resume</h1>
+     <h1 class="text-indigo-500 text-4xl font-bold text-center"  >Resume</h1>
     <div class=" border border-1 border-black ">
     <img  src="https://cdn.discordapp.com/attachments/944189291427430421/964783609896632320/demoResume.jpg"></img>
      </div>
