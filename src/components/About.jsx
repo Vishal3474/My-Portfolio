7e1304e57eb972d8713029b6ef3f6202dc9c477e
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-04-16 12:07:43 +0530

    Update About.jsx
    yes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,6 +22,12 @@ const About = () => {
             <dd className="mt-1 text-sm text-indigo-500 sm:mt-0 sm:col-span-2">VishalKumar3474@gmail.com</dd>
           </div>
         
+      <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Mobile No.-</dt>
+            <dd className="mt-1 text-sm text-indigo-500 sm:mt-0 sm:col-span-2">9520630066</dd>
+          </div>
+   
+   
           <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-500">About</dt>
             <dd className="mt-1 text-sm text-indigo-500 sm:mt-0 sm:col-span-2">
