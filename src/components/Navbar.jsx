3e2ae21c332369b0e8aad7f9e9fc22ed5684947f
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-04-16 20:14:01 +0530

    Update Navbar.jsx
    testing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,9 @@ const Navbar = () => {
     <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
-          <h1 className="text-3xl font-bold mr-4 sm:text-4xl">VISHAL.</h1>
+    
+    <img src="https://cdn.discordapp.com/attachments/944189291427430421/964898796561641512/logo.png" alt="logo" />
+          
           <ul className="hidden md:flex">
             <li>
               <Link to="home" smooth={true} duration={500}>
