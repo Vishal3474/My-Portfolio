88cba80fe81d6dea20f9d37c32835144de255d58
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-04-16 12:43:34 +0530

    Update Pricing.jsx
    ok

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -5,8 +5,8 @@ import { CheckIcon } from '@heroicons/react/solid';
 const Pricing = () => {
   return (
     <div name='pricing' className='w-full my-32'  >
-     <h1>hii testing</h1>
-    <img src="https://cdn.discordapp.com/attachments/944189291427430421/964783609896632320/demoResume.jpg"></img>
+     <h1 class="text-indigo-500 text-4xl font-bold  >Resume</h1>
+    <img  src="https://cdn.discordapp.com/attachments/944189291427430421/964783609896632320/demoResume.jpg"></img>
     </div>
   );
 };
